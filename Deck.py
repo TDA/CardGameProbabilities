97a6a79b631269e388e3648ac09bede6d7605e0b
Sai Pc <schand31@asu.edu>
2015-09-24 18:24:42 -0700

    corrected the program to obtain proper probability also modified the deck class to now be customizable (spanish etc)

--- Deck.py
+++ Deck.py
@@ -1,22 +1,33 @@
 __author__ = 'saipc'
 
 class Deck:
-    def __init__(self):
-        self.suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    def __init__(self,
+                 suits = ("Hearts", "Clubs", "Diamonds", "Spades"),
+                 normalCards = [x for x in xrange(2, 11, 1)],
+                 highCards = ("Ace", "King", "Queen", "Jack"),
+                 totalCards = 52):
+        self.suits = list(suits)
         # self.normalCards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
-        self.normalCards = [x for x in xrange(2, 11, 1)]
-        self.highCards = ["Ace", "King", "Queen", "Jack"]
-        self.totalCards = 52
+        self.normalCards = normalCards
+        self.highCards = list(highCards)
+        self.totalCards = totalCards
 
     def calcProbabOfCard (self, cardName):
         try:
             card, suit = cardName.split(" ")
             suit = suit.lower()
             if (suit in (suit.lower() for suit in self.suits)):
-                if isinstance(card, int) and int(card) in self.normalCards:
+                # see if the card is a number or a face card,
+                # and continue searching for it appropriately
+                try:
+                    card = int(card)
+                except:
+                    card = card
+                # was card a number?
+                if isinstance(card, int) and card in self.normalCards:
                     return 1.0 / self.totalCards
-                else:
-                    if (card  in (card.lower() for card in self.highCards)):
+                # was card a face card?
+                elif (card  in (card.lower() for card in self.highCards)):
                         return 1.0 / self.totalCards
             else:
                 return -1
@@ -34,3 +45,7 @@ deck = Deck()
 x = deck.calcProbabOfCard("8 hearts")
 y = deck.calcProbabOfCard("7 spades")
 print x , y
+
+halfdeck = Deck(("sai", "pc"))
+x = halfdeck.calcProbabOfCard("8 sai")
+print x
