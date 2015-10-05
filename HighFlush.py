cec755fc4bad4fb38310ae4d4b9ccaa2a7d00451
Sai Pc <schand31@asu.edu>
2015-10-05 13:49:56 -0700

    High card flush done

--- HighFlush.py
+++ HighFlush.py
@@ -8,4 +8,14 @@ if __name__ == '__main__':
     # we are dealt 7 cards, lets try calculating
     # the probabilities of 1-7 cards being same suit
     for x in xrange(1, 8):
-        print deck.calcProbability(x, 52)
+        # simple probab, assuming replacement,
+        # no joker, and 52 card deck, forget this.
+        y = x
+        P = 1
+        # complex probability, a la compound interest
+        # no replacement, real life scenario
+        while y > 0:
+            y = y - 1
+            # print 13 - y, 52.0 - y
+            P = P * deck.calcProbability(13 - y, 52.0 - y)
+        print "Getting", x, "cards of same suit", P
