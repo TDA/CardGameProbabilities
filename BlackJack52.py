32913ecf5bd62fa49f51b5ac21a1eff173d42ac9
Sai Pc <schand31@asu.edu>
2015-10-06 22:50:32 -0700

    Added comments

--- BlackJack52.py
+++ BlackJack52.py
@@ -1,6 +1,14 @@
 __author__ = 'saipc'
 from Deck import Deck
-
+# This pgm calculates the probability
+# of getting a blackjack, with no prior
+# info, and no memory
 
 if __name__ == '__main__':
+    # no jokers in bj, so this is fine
     deck = Deck()
+
+    # Lets do one deck, easier to count,
+    # and can replicate
+    # We might need a separate card
+    # counter class :)
